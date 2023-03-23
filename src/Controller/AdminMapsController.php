792293c870aeea0c55d08b9e11def493f6707d81
ByZirTox <xxtedyxxrenard@gmail.com>
2023-03-23 16:06:21 +0100

    Finish

--- src/Controller/AdminMapsController.php
+++ src/Controller/AdminMapsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\HunterRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminMapsController extends AbstractController
 {
     #[Route('/admin/maps', name: 'app_admin_maps')]
-    public function index(): Response
+    public function index(HunterRepository $hunterRepository): Response
     {
+
+
+        $markers =  $hunterRepository->findAll([]);
+
+
         return $this->render('admin_maps/index.html.twig', [
             'controller_name' => 'AdminMapsController',
+            'markers' => $markers,
+
         ]);
+    }
+
+    #[Route('/admin/maps/load', name: 'app_admin_maps_load')]
+    public function load(HunterRepository $hunterRepository): Response
+    {
+
+
+        $markers =  $hunterRepository->findAll([]);
+
+
+        return $this->json($markers);
     }
 }
