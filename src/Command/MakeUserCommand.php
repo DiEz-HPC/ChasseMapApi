29b5a0279358cdb568ec408e8f686d67abce2564
RedPandore <houry9@gmail.com>
2023-03-24 15:32:13 +0100

    useless use

--- src/Command/MakeUserCommand.php
+++ src/Command/MakeUserCommand.php
@@ -5,7 +5,6 @@ namespace App\Command;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputArgument;
